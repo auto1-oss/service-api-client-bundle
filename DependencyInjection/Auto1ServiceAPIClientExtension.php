5c51f5ba39bb47091ad7de647cdf5fad3c78971e
Ilya Antipenko <ilya@antipenko.pp.ua>
2021-06-21 18:49:09 +0200

    Remove request_stack service definition check (#16)

--- DependencyInjection/Auto1ServiceAPIClientExtension.php
+++ DependencyInjection/Auto1ServiceAPIClientExtension.php
@@ -26,14 +26,5 @@ class Auto1ServiceAPIClientExtension extends Extension
         $container->setParameter('auto1_service_api_client.request_visitors', $config['request_visitors']);
         $container->setParameter('auto1_service_api_client.propagate_headers', $config['propagate_headers']);
         $container->setParameter('auto1_service_api_client.request_time_log_level', $config['request_time_log_level']);
-
-        /*
-         * Headers to propagate are by default taken from @request_stack if exists
-         * so if there is no initial request - all related service definitions will be removed
-         */
-        if (!$container->hasDefinition('request_stack')) {
-            $container->removeDefinition('auto1.api.request.visitor.header_propagation');
-            $container->removeDefinition('auto1.api.previous_request');
-        }
     }
 }
